6eab41094b545e3ae569102e60fb49db13db655a
HoangThanhTung <90949869+tung2k1@users.noreply.github.com>
2025-05-22 16:35:04 +0700

    Update install_MegaCli.sh

--- install_MegaCli.sh
+++ install_MegaCli.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "👉 Bắt đầu cài đặt Zabbix Agent 2..."
+sudo dnf remove zabbix-agent2 -y
+sudo dnf remove zabbix-release -y
+sudo rm -f /etc/yum.repos.d/zabbix*.repo
 
 # Cài đặt Zabbix Agent 2
 rpm -Uvh https://repo.zabbix.com/zabbix/7.0/alma/8/x86_64/zabbix-release-latest-7.0.el8.noarch.rpm
