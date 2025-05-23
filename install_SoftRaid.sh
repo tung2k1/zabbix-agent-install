e9a92be88ff307e3957494b65a04f304a8b04812
HoangThanhTung <90949869+tung2k1@users.noreply.github.com>
2025-05-23 15:33:43 +0700

    Create install_SoftRaid.sh

---
+++ install_SoftRaid.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+echo "👉 Bắt đầu cài đặt Zabbix Agent 2..."
+sudo dnf remove zabbix-agent2 -y
+sudo dnf remove zabbix-release -y
+sudo rm -f /etc/yum.repos.d/zabbix*.repo
+
+# Cài đặt Zabbix Agent 2
+rpm -Uvh https://repo.zabbix.com/zabbix/7.0/alma/8/x86_64/zabbix-release-latest-7.0.el8.noarch.rpm
+dnf clean all
+dnf install -y zabbix-agent2 zabbix-agent2-plugin-* jq
+
+# Lấy địa chỉ IP hiện tại của server
+SERVER_IP=$(hostname -I | awk '{print $1}')
+STATIC_IP="103.253.21.236"
+echo "👉 IP của server: $SERVER_IP"
+echo "👉 Cấu hình Zabbix với Server=$STATIC_IP,$SERVER_IP và ServerActive=$STATIC_IP"
+
+# Cập nhật file cấu hình Zabbix Agent
+sed -i "s/^Server=.*/Server=$STATIC_IP,$SERVER_IP/" /etc/zabbix/zabbix_agent2.conf
+sed -i "s/^ServerActive=.*/ServerActive=$STATIC_IP/" /etc/zabbix/zabbix_agent2.conf
+sed -i "s/^Hostname=.*/Hostname=$SERVER_IP/" /etc/zabbix/zabbix_agent2.conf
+
+cat <<EOF >> /etc/zabbix/zabbix_agent2.conf
+UserParameter=raid.status.custom,/usr/local/bin/check_raid_status_custom.sh
+UserParameter=raid.pd_firmware_state,sudo /usr/local/bin/check_pd_firmware_state.sh
+EOF
+
+# Tạo script kiểm tra RAID
+echo "👉 Tạo script kiểm tra soft RAID..."
+cat <<'EOF' > /usr/local/bin/check_raid_status_custom.sh
+#!/bin/bash
+raid_arrays=$(cat /proc/mdstat | grep ^md | awk '{print $1}')
+total_failed_devices=0
+for array in $raid_arrays; do
+    failed_devices=$(sudo mdadm --detail /dev/$array | grep "Failed Devices" | awk '{print $4}')
+    total_failed_devices=$((total_failed_devices + failed_devices))
+done
+echo $total_failed_devices
+EOF
+
+
+
+
+# Cấp quyền thực thi cho các script
+chmod +x /usr/local/bin/check_raid_status_custom.sh
+
+# Thêm quyền sudo cho Zabbix để chạy mdadm mà không cần mật khẩu
+echo "👉 Cấu hình sudo cho Zabbix..."
+
+echo "zabbix ALL=(ALL) NOPASSWD: /usr/sbin/mdadm" | sudo tee -a /etc/sudoers > /dev/null
+
+# Mở cổng firewall cho Zabbix Agent
+echo "👉 Cấu hình firewall..."
+sudo firewall-cmd --permanent --add-port=10050/tcp
+sudo firewall-cmd --reload
+
+# Khởi động Zabbix Agent 2
+echo "👉 Khởi động lại Zabbix Agent 2..."
+systemctl enable --now zabbix-agent2
+systemctl restart zabbix-agent2
+systemctl status zabbix-agent2 --no-pager
+
+echo "✅ Cài đặt Zabbix Agent 2 hoàn tất!"
