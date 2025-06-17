2dfbd8a3e950bf3682035923dc33d1d7917e0aed
HoangThanhTung <90949869+tung2k1@users.noreply.github.com>
2025-06-17 14:31:37 +0700

    Update install_SoftRaid.sh

--- install_SoftRaid.sh
+++ install_SoftRaid.sh
@@ -6,9 +6,10 @@ echo "👉 Bắt đầu cài đặt Zabbix Agent 2..."
 #sudo rm -f /etc/yum.repos.d/zabbix*.repo
 
 # Cài đặt Zabbix Agent 2
-rpm -Uvh https://repo.zabbix.com/zabbix/7.0/alma/8/x86_64/zabbix-release-latest-7.0.el8.noarch.rpm
+rpm -Uvh https://repo.zabbix.com/zabbix/7.2/release/alma/9/noarch/zabbix-release-latest-7.2.el9.noarch.rpm
 dnf clean all
-dnf install -y zabbix-agent2 zabbix-agent2-plugin-* jq
+dnf install zabbix-agent2
+dnf install zabbix-agent2-plugin-mongodb zabbix-agent2-plugin-mssql zabbix-agent2-plugin-postgresql
 
 # Lấy địa chỉ IP hiện tại của server
 SERVER_IP=$(hostname -I | awk '{print $1}')
