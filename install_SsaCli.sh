91d5fb8abe1e9cc81f2a49d1beca1f4ec0b64add
HoangThanhTung <90949869+tung2k1@users.noreply.github.com>
2025-06-17 17:13:55 +0700

    Update install_SsaCli.sh

--- install_SsaCli.sh
+++ install_SsaCli.sh
@@ -8,12 +8,14 @@ sudo rm -rf /etc/zabbix
 sudo rm -rf /var/log/zabbix
 
 # Cài đặt Zabbix Agent 2
-rpm -Uvh https://repo.zabbix.com/zabbix/6.4/rhel/8/x86_64/zabbix-release-6.4-2.el8.noarch.rpm
+rpm -Uvh https://repo.zabbix.com/zabbix/7.2/release/alma/8/noarch/zabbix-release-latest-7.2.el8.noarch.rpm
 dnf clean all
-dnf install -y zabbix-agent2 zabbix-agent2-plugin-* jq
+dnf install zabbix-agent2 -y
+dnf install zabbix-agent2-plugin-mongodb -y zabbix-agent2-plugin-mssql -y zabbix-agent2-plugin-postgresql -y
+
 # Lấy địa chỉ IP hiện tại của server
 SERVER_IP=$(hostname -I | awk '{print $1}')
-STATIC_IP="103.253.21.236"
+STATIC_IP="203.188.166.239"
 echo "👉 IP của server: $SERVER_IP"
 echo "👉 Cấu hình Zabbix với Server=$STATIC_IP,$SERVER_IP và ServerActive=$STATIC_IP"
 
